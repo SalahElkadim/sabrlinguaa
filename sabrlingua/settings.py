b65c9e638d9bfd2c574263998fe6103271a2b8ac
unknown <khaledgaber494@gmail.com>
2025-12-09 11:31:04 +0200

    first commit

--- sabrlingua/settings.py
+++ sabrlingua/settings.py
@@ -145,7 +145,7 @@ WSGI_APPLICATION = 'sabrlingua.wsgi.application'
 # استخدام dj_database_url للتعامل مع SQLite و PostgreSQL
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
