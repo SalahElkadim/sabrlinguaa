56080f6ae30ec07a1e01b030f8f6157e618621b2
unknown <khaledgaber494@gmail.com>
2026-04-07 13:41:46 +0200

    first commit

--- sabrlingua/settings.py
+++ sabrlingua/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'ielts',
     'step',
     'booking',
+    'django_celery_results',
 
 ]
 
@@ -267,3 +268,12 @@ CSRF_TRUSTED_ORIGINS = [
     'https://sabrlinguaa-production.up.railway.app',
     'https://*.railway.app',  # للسماح بكل subdomains
 ]
+
+# Celery
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Riyadh'
+
