0b5f92baa91135848677c8e779425fab5400dbcb
unknown <khaledgaber494@gmail.com>
2026-05-23 14:21:02 +0300

    apple pay

--- sabrlingua/urls.py
+++ sabrlingua/urls.py
@@ -1,6 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+# myproject/urls.py
+from django.http import FileResponse, HttpResponse
+from django.urls import path
+from django.conf import settings
+import os
+
+def apple_pay_domain_association(request):
+    file_path = os.path.join(settings.BASE_DIR, '.well-known', 
+                             'apple-developer-merchantid-domain-association')
+    with open(file_path, 'rb') as f:
+        return HttpResponse(f.read(), content_type='application/octet-stream')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,9 +23,8 @@ urlpatterns = [
     path('step/', include('step.urls')),
     path('booking/', include('booking.urls')),
     path('esp/', include('esp.urls')),
-
     path('general/', include('general.urls', namespace='general')),
-
+    path('.well-known/apple-developer-merchantid-domain-association',apple_pay_domain_association),
 
 
 
